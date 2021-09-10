4f84dd0d84f0311bae8ae0bff4406fa261fda3b8
solowav3 <fil.solovjov@gmail.com>
2021-09-10 14:15:31 +0200

    Events basics

--- src/index.js
+++ src/index.js
@@ -38,11 +38,24 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
+  // attribute, eventhandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world!");
+  };
+  const complexExample = (author) => {};
+  console.log(author);
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        First
+      </button>
+      <button type="button" onClick={complexExample(author)}>
+        Second
+      </button>
     </article>
   );
 };
